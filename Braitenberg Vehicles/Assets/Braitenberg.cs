715e178d6e8a81b15adb8e7bb37da864c2ab9f1f
msnyder102 <msnyder102@gmail.com>
2015-04-09 23:49:14 -0500

    Reorganized + Started vehicles project

---
+++ Braitenberg Vehicles/Assets/Braitenberg.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Braitenberg : MonoBehaviour
+{
+
+    public Rigidbody leftWheel;
+    public Rigidbody rightWheel;
+    public Transform body;
+
+    public Transform leftSensor;
+    public Transform rightSensor;
+
+    public List<Transform> lights = new List<Transform>();
+
+    //gotta do k matrix in a stupid way to see it in inspector...
+    [System.Serializable]
+    public class KMatrix{
+        public float K11 = 0;
+        public float K12 = 0;
+        public float K21 = 0;
+        public float K22 = 0;
+    }
+    public KMatrix k;
+    public float speedMultiplier;
+    // Use this for initialization
+    void Start()
+    {
+        //find all light sources for this simulation
+        foreach(GameObject l in GameObject.FindGameObjectsWithTag("Light"))
+        {
+            lights.Add(l.transform);
+        }
+    }
+
+    void Update()
+    {
+        float leftSensorValue = 0;
+        float rightSensorValue = 0;
+        foreach (Transform l in lights)
+        {
+            leftSensorValue += 100 / Vector3.Distance(new Vector3(l.position.x, leftSensor.position.y, l.position.z), leftSensor.position);
+            rightSensorValue += 100 / Vector3.Distance(new Vector3(l.position.x, rightSensor.position.y, l.position.z), rightSensor.position);
+            //Debug.Log("Left : " +leftSensorValue + ", Right : " + rightSensorValue);
+        }
+        leftWheel.velocity = body.forward * speedMultiplier * (k.K11 * leftSensorValue + k.K12 * rightSensorValue);
+        rightWheel.velocity = body.forward * speedMultiplier * (k.K21 * leftSensorValue + k.K22 * rightSensorValue);
+        //Debug.Log("Left : " + leftWheel.velocity.magnitude + ", Right : " + rightWheel.velocity.magnitude);
+    }
+}
---
+++ Braitenberg Vehicles/Assets/WarpWall.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpWall : MonoBehaviour
+{
+
+    public Transform otherWall;
+    public enum wallType
+    {
+        horizontal,
+        vertical
+    }
+    public wallType type;
+    public float warpOffset;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("VehicleBody"))
+        {
+            if (type == wallType.horizontal)
+            {
+                //gotta shift all 3 parts by same amount.
+                //other = vehicle body
+                other.transform.position = new Vector3(otherWall.position.x  + warpOffset, other.transform.position.y, other.transform.position.z);
+                //wheels, ugly but who cares.
+                Transform w1 = other.transform.parent.GetChild(0).transform;
+                Transform w2 = other.transform.parent.GetChild(1).transform;
+                w1.position = new Vector3(otherWall.position.x + warpOffset, w1.position.y, w1.position.z);
+                w2.position = new Vector3(otherWall.position.x + warpOffset, w2.position.y, w2.position.z);
+            }
+            else
+            {
+                //gotta shift all 3 parts by same amount.
+                //other = vehicle body
+                other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, otherWall.position.z + warpOffset);
+                //wheels, ugly but who cares.
+                Transform w1 = other.transform.parent.GetChild(0).transform;
+                Transform w2 = other.transform.parent.GetChild(1).transform;
+                w1.position = new Vector3(w1.position.x, w1.position.y, otherWall.position.z + warpOffset);
+                w2.position = new Vector3(w2.position.x, w2.position.y, otherWall.position.z + warpOffset);
+            }
+        }
+    }
+}
